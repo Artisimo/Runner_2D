f70833e81df468bc72a84ffca36d788004ddc31e
Artis Birznieks <birznieks.artis@gmail.com>
2022-05-23 18:23:26 +0300

    Testing jumping functionality

--- src/Game.java
+++ src/Game.java
@@ -24,8 +24,13 @@ public class Game extends Canvas implements Runnable {
         this.addKeyListener(new KeyInput(handler));
 
         handler.addObject(new Player(180, 200, 32, 32, ID.Player, handler));
-        handler.addObject(new Platform(20,200, 100, 100, ID.Platform, handler));
-        handler.addObject(new Platform(20,350, 200, 100, ID.Platform, handler));
+        handler.addObject(new Platform(0,300, 100, 100, ID.Platform, handler));
+        handler.addObject(new Platform(0,400, 100, 100, ID.Platform, handler));
+        handler.addObject(new Platform(100,400, 100, 100, ID.Platform, handler));
+        handler.addObject(new Platform(200,250, 100, 100, ID.Platform, handler));
+        handler.addObject(new Platform(200,400, 100, 100, ID.Platform, handler));
+        handler.addObject(new Platform(300,400, 100, 100, ID.Platform, handler));
+        handler.addObject(new Platform(400,400, 100, 100, ID.Platform, handler));
     }
 
     public synchronized void start(){
