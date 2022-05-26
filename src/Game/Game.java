1e128de36516a888aa1def652ac036d7dc738111
Bosni4ko <stefanbosnyak9@gmail.com>
2022-05-26 15:09:51 +0300

    First music

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -48,6 +48,8 @@ public class Game extends Canvas implements Runnable {
         tex = new Texture();
 
         gameState = GameState.MENU;
+        sound.playMusic();
+
     }
 
     public void play(){
