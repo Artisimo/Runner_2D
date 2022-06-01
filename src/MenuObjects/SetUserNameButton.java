afe5f08f7085f0abf854f5a320a47877c5f55905
Artis Birznieks <birznieks.artis@gmail.com>
2022-06-01 13:05:27 +0300

    Added username length validation

--- src/MenuObjects/SetUserNameButton.java
+++ src/MenuObjects/SetUserNameButton.java
@@ -8,20 +8,23 @@ import java.sql.SQLException;
 
 public class SetUserNameButton extends MenuObject{
 
+    private boolean userNameValid;
     private Texture tex = Game.getInstance();
     public SetUserNameButton(int x, int y, MenuObjectID id, int width, int height) {
         super(x, y, id, width, height);
+        userNameValid = true;
     }
 
     @Override
     public void onClick(Game game) throws SQLException {
         if(game.userName.length() > 3 && game.userName.length() < 25 ){
-
+            userNameValid = true;
             game.gameState = GameState.MENU;
 
             Game.logger.info("Username set: " + game.userName);
             game.userId = mySqlDatabase.getUserID(game.userName);
         }else{
+            userNameValid = false;
             Game.logger.warn("The entered username is not valid");
         }
     }
@@ -33,6 +36,12 @@ public class SetUserNameButton extends MenuObject{
 
     @Override
     public void render(Graphics g) {
+        if(!userNameValid){
+            g.setColor(Color.RED);
+            g.setFont(new Font("TimesRoman", Font.PLAIN, 30));
+            int textWidth = g.getFontMetrics().stringWidth("Please enter a valid user name!");
+            g.drawString("Please enter a valid user name!", Game.WIDTH / 2 - textWidth / 2, y + 90);
+        }
         g.drawImage(tex.buttonImages[0], x, y, null);
     }
 }
